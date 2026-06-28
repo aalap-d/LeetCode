a52b8941fd4417de9965528323abead01ca05cf7
Aalap Biswas <157967555+aalap-d@users.noreply.github.com>
2026-06-29 00:13:43 +0530

    Time: 2 ms (97.29%) | Memory: 47 MB (83.74%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -6,7 +6,7 @@ class Solution {
         int[] rowCount = new int[m];
         int[] colCount = new int[n];
         
-        // 1. Count 1s in each row and column
+       
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 1) {
@@ -18,11 +18,11 @@ class Solution {
         
         int specialPositions = 0;
         
-        // 2. Identify the special positions
+        
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 1) {
-                    // Check if this is the only 1 in its row and column
+                   
                     if (rowCount[i] == 1 && colCount[j] == 1) {
                         specialPositions++;
                     }
