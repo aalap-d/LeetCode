281d870c0610f282e6da31df4d99161592f94a52
Aalap Biswas <157967555+aalap-d@users.noreply.github.com>
2026-08-02 01:00:22 +0530

    Time: 0 ms (100%), Space: 42.8 MB (41.06%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        if (n % 2 == 0) return true;
+        
+        int[] dp = nums.clone();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        
+        return dp[n - 1] >= 0;
+    }
+}
